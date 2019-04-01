ffe878ecbddc1762a333c35136071dace47d6671
bandivenkatsaicharan@gmail.com <bandivenkatsaicharan@gmail.com>
2019-04-01 17:00:25 +0530

    Getting Web3 Installed

--- compile.js
+++ compile.js
@@ -6,22 +6,3 @@ const LotteryPath = path.resolve(__dirname,'contracts','Lottery.sol');
 const source = fs.readFileSync(LotteryPath, 'utf8');
 
 module.exports = solc.compile(source,1).contracts[':Lottery'];
-
-
-
-
-/*console.log(JSON.parse(solc.compile(JSON.stringify({
-  language: 'Solidity',
-  sources: {
-    'lottery.sol': {
-      content: source,
-    },
-  },
-  settings: {
-    outputSelection: {
-      '*': {
-        '*': ['evm', 'bytecode'],
-      },
-    },
-  },
-}))).contracts['lottery.sol'].Lottery);*/
